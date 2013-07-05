91f7c89be209495b2f967870e28946e8c0806e8b
Dirkjan Ochtman <dirkjan@ochtman.nl>
2013-07-05 13:44:48 +0200

    Include timezone data.
    --HG--
    rename : index.html => index.tmpl

---
+++ Makefile
@@ -0,0 +1,12 @@
+
+index.html: pruned.json index.tmpl
+	python create.py pruned.json > index.html
+
+pruned.json: source.json prune.py
+	python prune.py source.json > pruned.json
+
+source.json: zones.py
+	python zones.py $(shell ls /usr/portage/distfiles/tzdata20* | tail -n1) > $@
+
+clean:
+	rm -rf index.html *.json
---
+++ create.py
@@ -0,0 +1,12 @@
+import simplejson as json
+import jinja2
+
+if __name__ == '__main__':
+	
+	data = {}
+	with open('pruned.json') as f:
+		data['zones'] = json.load(f)
+	
+	env = jinja2.Environment(loader=jinja2.FileSystemLoader('.'))
+	env.filters['json'] = lambda x: json.dumps(x)
+	print env.get_template('index.tmpl').render(**data)
